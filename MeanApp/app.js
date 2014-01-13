54c54e5bd1cc316aa02eaf3c4dcc113138bc31b4
daejoon <kkode1911@gmail.com>
2014-01-13 19:41:12 +0900

    git commit

--- MeanApp/app.js
+++ MeanApp/app.js
@@ -5,7 +5,6 @@
 
 var express = require('express');
 var routes  = require('./routes');
-var user    = require('./routes/user');
 var http    = require('http');
 var path    = require('path');
 var util    = require('util');
@@ -30,7 +29,8 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/users', routes.user);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- MeanApp/routes/index.js
+++ MeanApp/routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.user = function(req, res){
+    res.send("respond with a resource");
+};
